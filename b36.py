034e99f8eb6c37ed5eb3c1435637b4bfe3da6a8f
Duong Phong <chuduongphong47@gmail.com>
2026-09-03 19:56:11 +0700

    Refactor code to remove commented-out lines for clarity

--- b36.py
+++ b36.py
@@ -1,14 +1,14 @@
 # Liet ke cac so nguyen to
-# a = int(input())
-# b = int(input())
-# def so_nguyen_to(number):
-#     if number < 2:
-#         return False
-#     for i in range(2, number):
-#         if number % i == 0:
-#             return False
-#     return True
-# for i in range(a, b+1):
-#     if so_nguyen_to(i):
-#         print(i)]
-
+a = int(input())
+b = int(input())
+def so_nguyen_to(number):
+    if number < 2:
+        return False
+    for i in range(2, number):
+        if number % i == 0:
+            return False
+    return True
+for i in range(a, b+1):
+    if so_nguyen_to(i):
+        print(i)
+        
