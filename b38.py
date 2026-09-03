594f1ad12a238affecf81b94265f6eeb344ac2b7
Duong Phong <chuduongphong47@gmail.com>
2026-09-03 20:10:05 +0700

    Add function to check the greatest common divisor (GCD) of two numbers

---
+++ b38.py
@@ -0,0 +1,12 @@
+a = int(input())
+b = int(input())
+def kiemtraucln(a,b):
+    ucln = 1
+    for i in range(1, a + 1):
+        if a % i ==0 and b % i == 0:
+            ucln = i
+
+    if ucln == 1:
+        print("Dung")
+    else:
+        print("Sai")
